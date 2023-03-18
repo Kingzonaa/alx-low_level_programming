625dd62f877600c9524d788a3e2251015fa627bc
kingzonaa <legalbasin.com>
2023-03-17 21:40:03 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+[200~#include <stdio.h>
+/*
+ *  * main - entry point
+ *   *
+ *    * Return: 0
+ *    */
+int main(void)
+{       
+char number = '0';
+while (number <= '9')
+{
+putchar(number);
+number++;
+}
+putchar('\n');
+return (0);
+}
