6a9e8eeba0fbc2ffb54e70a8fdb6c10f72e61558
kingzonaa <legalbasin.com>
2023-03-18 02:50:39 -0700

    task5and5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
-[200~#include <stdio.h>
-/*
- *  * main - entry point
- *   *
- *    * Return: 0
-*/
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{       
-char number = '0';
-while (number <= '9')
 {
-putchar(number);
-number++;
-}
-putchar('\n');
-return (0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
 }
