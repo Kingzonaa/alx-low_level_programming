6840b89665ecc176ffa3ff81c8929e9346bc89eb
kingzonaa <legalbasin.com>
2023-03-18 02:44:58 -0700

    task7and7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-/*
- * main - entry point
- *
- * Return: 0
-*/
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{       
-char letter = 122;
-while (letter >= 97)
 {
-putchar(letter);
-letter--;
-}
-putchar('\n');
-return (0);
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
