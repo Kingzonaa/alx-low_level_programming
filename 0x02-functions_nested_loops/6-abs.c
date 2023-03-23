ac32f12263dfc0450453d6a8fc99abadca558882
kingzonaa <legalbasin.com>
2023-03-22 20:49:55 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute for absolute value of an integer
+ * @n: The parameter to compute for absolute value
+ * Return: positive value equal to n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
