92c825a8affb1c33dbd273aa0ce075a499f0b524
kingzonaa <legalbasin.com>
2023-03-22 23:14:48 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - this function will add two integers and return the result.
+ * @a: first interger
+ * @b: second intenger
+ * Return: the sum result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
