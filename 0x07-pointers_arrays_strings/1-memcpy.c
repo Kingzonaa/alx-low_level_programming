70d9ab0b9fadd93e23ea798d52c6c5b3ba40fbc9
kingzonaa <legalbasin.com>
2023-04-03 18:09:51 -0700

    task2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,17 @@
-#include 
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *ptr_dest = dest;
+char *ptr_src = src;
+
+while (n--)
+{
+*ptr_dest++ = *ptr_src++;
+return (0);
+}
